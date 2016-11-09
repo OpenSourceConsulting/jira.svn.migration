dbedf72ec5c559b8d78b641b75e4f52183acee29
hptvl <phuctran1107@gmail.com>
2016-11-09 17:52:17 +0900

    edit upload function

--- synteg/src/main/webapp/resources/js/jira.svn.js
+++ synteg/src/main/webapp/resources/js/jira.svn.js
@@ -192,31 +192,6 @@ function doSubmit() {
 	formData.append("isExtract", $("#extractZip").is(":checked"));
 	var url = CONTEXT_PATH + "/api/svn/import";
 	$.ajax({
-		xhr : function() {
-			var xhr = new window.XMLHttpRequest();
-			xhr.upload.addEventListener("progress", function(evt) {
-				if (evt.lengthComputable) {
-					var percentComplete = evt.loaded / evt.total;
-					console.log(percentComplete);
-					$('.progress').css({
-						width : percentComplete * 100 + '%'
-					});
-					if (percentComplete === 1) {
-						$('.progress').addClass('hide');
-					}
-				}
-			}, false);
-			xhr.addEventListener("progress", function(evt) {
-				if (evt.lengthComputable) {
-					var percentComplete = evt.loaded / evt.total;
-					console.log(percentComplete);
-					$('.progress').css({
-						width : percentComplete * 100 + '%'
-					});
-				}
-			}, false);
-			return xhr;
-		},
 		type : "POST",
 		url : url,
 		data : formData,
